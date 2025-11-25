376f46e5cd2fb37da3febe24ee4a09bfbe39e765
Rujing Xu <delia.rj.xu@outlook.com>
2025-11-25 10:28:28 +0000

    LC-squares-of-a-sorted-array [Runtime: 1 ms; Memory: 47.4 MB]

---
+++ array/squares-of-a-sorted-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int left=0;
+        int right=nums.length-1;
+        int[] result=new int[nums.length];
+        int index=result.length-1;
+
+        while(left<=right){
+            if(nums[left]*nums[left]>nums[right]*nums[right]){
+                result[index--]=nums[left]*nums[left];
+                ++left;
+            }else{
+                result[index--]=nums[right]*nums[right];
+                --right;
+            }
+        }
+      return result;       
+    }
+}
