d601d2fefe9eb39197a66ef2e4a2dd991b7fb405
Rujing Xu <delia.rj.xu@outlook.com>
2026-01-24 20:36:26 +0000

    LC-length-of-last-word [Runtime: N/A; Memory: N/A]

---
+++ string/length-of-last-word.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        //1、从字符串末尾开始
+        int length=0;
+        int i=s.length()-1;
+
+        //2、跳过队尾所有空格
+        if(i>=0&&s.charAt(i)==' '){
+            i--;
+        }
+
+        //3、找到最后一个单词
+        if(i>=0&&s.charAt(i) !==' '){
+            i--;
+            length++;
+        }
+        return length;
+        
+    }
+}
