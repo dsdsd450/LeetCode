1d33fdc58039b17c3f2b3e4cf1c3561d93c35dfc
Rujing Xu <delia.rj.xu@outlook.com>
2026-01-24 20:44:45 +0000

    LC-length-of-last-word [Runtime: 0 ms; Memory: 43 MB]

--- string/length-of-last-word.java
+++ string/length-of-last-word.java
@@ -5,12 +5,12 @@ class Solution {
         int i=s.length()-1;
 
         //2、跳过队尾所有空格
-        if(i>=0&&s.charAt(i)==' '){
+        while(i>=0&&s.charAt(i)==' '){
             i--;
         }
 
         //3、找到最后一个单词
-        if(i>=0&&s.charAt(i) !=' '){
+        while(i>=0&&s.charAt(i) !=' '){
             i--;
             length++;
         }
