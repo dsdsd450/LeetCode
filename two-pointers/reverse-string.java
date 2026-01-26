d26ede82482d00d04f953703baa3fa7464bf71c0
Rujing Xu <delia.rj.xu@outlook.com>
2026-01-26 15:13:09 +0000

    LC-reverse-string [Runtime: 0 ms; Memory: 48.6 MB]

--- two-pointers/reverse-string.java
+++ two-pointers/reverse-string.java
@@ -1,12 +1,12 @@
 class Solution {
     public void reverseString(char[] s) {
-        int left=0,right=s.length-1;
+       int left=0,right=s.length-1;
 
-        while(left<right){
-        char tmp = s[left];
+       while(left<right){
+        char tmp= s[left];
         s[left++]=s[right];
         s[right--]=tmp;
-        }
+       }
 
     }
 }
