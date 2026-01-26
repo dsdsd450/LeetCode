8ea503c8c623cc3f567054b8ae496478502b84a4
Rujing Xu <delia.rj.xu@outlook.com>
2026-01-26 15:49:39 +0000

    LC-reverse-words-in-a-string [Runtime: 8 ms; Memory: 43.9 MB]

--- string/reverse-words-in-a-string.java
+++ string/reverse-words-in-a-string.java
@@ -1,27 +1,30 @@
 class Solution {
     public String reverseWords(String s) {
-        //Convert String to StringBuilder for easier manipulation
+        //convert String into StringBuilder for easier manipulation
         StringBuilder sb=trimSpaces(s);
 
-        //1.reverse the entire string
         reverse(sb,0,sb.length()-1);
 
-        //2.reverse each individual word
         reverseEachWord(sb);
 
         return sb.toString();
     }
 
-    private StringBuilder trimSpaces(String s){
+    public StringBuilder trimSpaces(String s){
+        //把左右空格去掉
         int left=0,right=s.length()-1;
+        while(left<=right && s.charAt(left) ==' ') left++;
+        while(left<=right && s.charAt(right) ==' ') right--;
 
-        while(left<=right && s.charAt(left)==' ')left++;
-        while(left<=right && s.charAt(right)==' ')right--;
+        //中间空格只剩一格
+        StringBuilder sb = new StringBuilder();
+        
 
-        StringBuilder sb=new StringBuilder();
-        while( left<=right){
+        while(left<=right){
             char c=s.charAt(left);
-            if(c !=' ' || sb.charAt(sb.length()-1) !=' '){
+            if(c !=' '){
+                sb.append(c);
+            }else if(sb.charAt(sb.length()-1) !=' '){
                 sb.append(c);
             }
             left++;
@@ -29,21 +32,25 @@ class Solution {
         return sb;
     }
 
-    private void reverse(StringBuilder sb,int left,int right){
-        while(left < right){
-            char temp=sb.charAt(left);
+    //把整个string reverse
+    private void reverse(StringBuilder sb, int left, int right){
+        while(left<right){
+            char tmp=sb.charAt(left);
             sb.setCharAt(left++,sb.charAt(right));
-            sb.setCharAt(right--,temp);
+            sb.setCharAt(right--,tmp);
         }
     }
 
+    //把每个单词reverse
     private void reverseEachWord(StringBuilder sb){
         int start=0,end=0;
-        int n=sb.length();
+        int n=sb.length()-1;
 
         while(start<n){
-            while(end<n && sb.charAt(end)!=' ') end++;
+            while(end<=n && sb.charAt(end) != ' ') end++;
+
             reverse(sb,start,end-1);
+
             start=end+1;
             end=start;
         }
