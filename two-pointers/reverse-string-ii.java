09960967cc992eedcd51074a3c8fb090811f24db
Rujing Xu <delia.rj.xu@outlook.com>
2026-01-26 16:55:57 +0000

    LC-reverse-string-ii [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/reverse-string-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String reverseStr(String s, int k) {
+        StringBuilder sb= new StringBuilder(s);
+        int n=sb.length();
+
+        int q = n / (2*k);
+        int remainder = n % (2*k);
+
+        for(int i=0;i<=q-1;i++){
+            reverse(sb,0+k*i,k*(i+1)-1);
+        }
+
+        if(remainder < k && remainder>0) reverse(sb,2*k*q,n-1);
+        else if(remainder>=k && remainder< 2*k) reverse(sb,2*k*q,2*k*q+k-1);
+
+        return sb.toString();
+    }
+
+    private void reverse(StringBuilder sb,int left,int right){
+        while(left<right){
+            char tmp=sb.charAt(left);
+            sb.setCharAt(left++,sb.charAt(left));
+            sb.setCharAt(right--,tmp);
+        }
+    }
+}
