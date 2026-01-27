2fe9c93032a4c4ae7bdebb0c6250b366eecf7d3f
Rujing Xu <delia.rj.xu@outlook.com>
2026-01-27 17:14:45 +0000

    LC-binary-search [Runtime: 0 ms; Memory: 48.3 MB]

--- binary-search/binary-search.java
+++ binary-search/binary-search.java
@@ -1,11 +1,14 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n=nums.length;
+        int left=0,right=nums.length-1;
 
-       for(int i=0;i<n;i++){
-        if (target == nums[i]) return i;
-       }
+        while(left<=right){
+            int mid=left+(right-left)/2;
 
-       return -1;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) left=mid+1;
+            else right=mid-1;
+        }
+        return -1;
 }
 }
