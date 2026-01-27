115b7da179e1a111501a22da48733bf0b7209cfc
Rujing Xu <delia.rj.xu@outlook.com>
2026-01-27 15:48:52 +0000

    LC-reverse-string-ii [Runtime: 1 ms; Memory: 44.7 MB]

--- two-pointers/reverse-string-ii.java
+++ two-pointers/reverse-string-ii.java
@@ -1,24 +1,24 @@
 class Solution {
     public String reverseStr(String s, int k) {
-        StringBuilder sb= new StringBuilder(s);
-        int n=sb.length();
+       StringBuilder sb=new StringBuilder(s);
+       int n =sb.length();
 
-        int q = n / (2*k);
-        int remainder = n % (2*k);
+       int q=n / (2*k);
+       int remainder= n % (2*k);
 
-        for(int i=0;i<=q-1;i++){
-            reverse(sb,0+2*k*i,k+2*k*i-1);
-        }
+       for(int i=0;i<q;i++){
+        reverse(sb,0+(2*k)*i,k+(2*k)*i-1);
+       }
 
-        if(remainder < k && remainder>0) reverse(sb,2*k*q,n-1);
-        else if(remainder>=k && remainder< 2*k) reverse(sb,2*k*q,2*k*q+k-1);
+       if(remainder<k &&remainder >0) reverse(sb,2*k*q,n-1);
+       else if(remainder>=k && remainder<(2*k)) reverse(sb,2*k*q,2*k*q+k-1);
 
-        return sb.toString();
+       return sb.toString();
     }
 
-    private void reverse(StringBuilder sb,int left,int right){
-        while(left<=right){
-            char tmp=sb.charAt(left);
+    private void reverse(StringBuilder sb, int left,int right){
+        while(left<right){
+            char tmp= sb.charAt(left);
             sb.setCharAt(left++,sb.charAt(right));
             sb.setCharAt(right--,tmp);
         }
