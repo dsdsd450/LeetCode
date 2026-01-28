a68b75b750f987291c63b5524b7acc1e3583fb26
Rujing Xu <delia.rj.xu@outlook.com>
2026-01-28 15:25:32 +0000

    LC-squares-of-a-sorted-array [Runtime: 1 ms; Memory: 47.1 MB]

--- two-pointers/squares-of-a-sorted-array.java
+++ two-pointers/squares-of-a-sorted-array.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int left=0;
-        int right=nums.length-1;
-        int[] result=new int[nums.length];
-        int index=result.length-1;
+       int left=0, right=nums.length-1;
+       int[] result=new int [nums.length];
+       int index=result.length-1;
 
-        while(left<=right){
-            if(nums[left]*nums[left]>nums[right]*nums[right]){
-                result[index--]=nums[left]*nums[left];
-                ++left;
-            }else{
-                result[index--]=nums[right]*nums[right];
-                --right;
-            }
+       while(left<=right){
+        if(nums[left]*nums[left]>nums[right]*nums[right]){
+            result[index--]=nums[left]*nums[left];
+            ++left;
         }
-      return result;       
+        else{
+            result[index--]=nums[right]*nums[right];
+            --right;
+        }
+       }
+       return result;
     }
 }
