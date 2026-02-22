c768642cebee69323c4045c9000c11a1941fc203
Rujing Xu <delia.rj.xu@outlook.com>
2026-02-22 11:41:04 +0000

    LC-find-all-anagrams-in-a-string [Runtime: 8 ms; Memory: 47.2 MB]

--- sliding-window/find-all-anagrams-in-a-string.java
+++ sliding-window/find-all-anagrams-in-a-string.java
@@ -1,27 +1,28 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-       List<Integer> result= new ArrayList<>();
-       if(s.length()<p.length()) return result;
+      List<Integer> result=new ArrayList<>();
+      if(s.length()<p.length()) return result;
 
-       int[] pCount=new int[26];
-       int[] windowCount=new int[26];
+      int[] pCount=new int[26];
+      int[] windowCount=new int[26];
 
-       for(int i=0;i<p.length();i++){
+      for(int i=0;i<p.length();i++){
         pCount[p.charAt(i)-'a']++;
         windowCount[s.charAt(i)-'a']++;
-       }
+      }
 
-       if(Arrays.equals(pCount,windowCount)) result.add(0);
+      if(Arrays.equals(pCount,windowCount)) result.add(0);
 
-       int n=s.length();
-       int m=p.length();
-       
-       for(int i=m;i<n;i++){
+      int n=s.length();
+      int m=p.length();
+
+      for(int i=m;i<n;i++){
         windowCount[s.charAt(i)-'a']++;
         windowCount[s.charAt(i-m)-'a']--;
 
         if(Arrays.equals(pCount,windowCount)) result.add(i-m+1);
-       }
-       return result;
+      }
+
+      return result;
     }
 }
